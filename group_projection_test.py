db7e4b79e9d53dff8a6f776741df676218f83edf
EXCurryBar <abo880805@gmail.com>
2024-01-31 21:50:06 +0800

    fix x y mismatch

--- group_projection_test.py
+++ group_projection_test.py
@@ -1,41 +1,35 @@
 import json
 import matplotlib.pyplot as plt
 import numpy as np
-
-from scipy.stats import entropy
-
-
-# import time
-# import os
-# import pprint
+# from scipy.stats import entropy
 
 
-def stack_frames(frame):
-    return np.dstack(frame)
+# def stack_frames(frame):
+#     return np.dstack(frame)
 
 
-def calculate_entropy(stacked_frames):
-    entropy_map = np.apply_along_axis(lambda x: entropy(x, base=10), axis=2, arr=stacked_frames)
-    return entropy_map
+# def calculate_entropy(stacked_frames):
+#     entropy_map = np.apply_along_axis(lambda x: entropy(x, base=10), axis=2, arr=stacked_frames)
+#     return entropy_map
 
 
-def mse(a, b):
-    err = np.sum((a - b) ** 2)
-    err /= bins ** 2
-    return err
+# def mse(a, b):
+#     err = np.sum((a - b) ** 2)
+#     err /= bins ** 2
+#     return err
 
 
-def plot_heatmap(entropy_map):
-    plt.cla()
-    plt.imshow(entropy_map, interpolation='nearest')
-    plt.draw()
+# def plot_heatmap(entropy_map):
+#     plt.cla()
+#     plt.imshow(entropy_map, interpolation='nearest')
+#     plt.draw()
 
 
-def calculate_pmf(x):
-    total = len(x)
-    set_x = list(set(x))
-    pmf = [x.count(item) / total for item in set_x]
-    return pmf
+# def calculate_pmf(x):
+#     total = len(x)
+#     set_x = list(set(x))
+#     pmf = [x.count(item) / total for item in set_x]
+#     return pmf
 
 
 data = json.load(open("output_file/wei_forward.json", 'r'))
@@ -56,39 +50,40 @@ for i in range(len(data)):
     # for _ in range(start_index, i):
     for g in data[i][1]["group"]:
         group = np.array(g).T
-        xs = list(group[0])
-        ys = list(group[1])
-        h, q_x, q_y = np.histogram2d(xs, ys, bins=bins, range=np.array([(-3, 3), (-3, 3)]))
-        y_0 = 0
-        x_0 = 0
-        for row in h:
-            if all(item <= 0.2 for item in row):
-                y_0 += 1
+        xs += list(group[0])
+        ys += list(group[1])
+    h, q_x, q_y = np.histogram2d(xs, ys, bins=bins, range=np.array([(-3, 3), (-3, 3)]))
+    y_0 = 0
+    x_0 = 0
+    for row in h:
+        if all(item <= 0.2 for item in row):
+            x_0 += 1
 
-        y_percentage = y_0 / bins
+    x_percentage = x_0 / bins
 
-        for row in h.T:
-            if all(item <= 0.2 for item in row):
-                x_0 += 1
+    for row in h.T:
+        if all(item <= 0.2 for item in row):
+            y_0 += 1
 
-        x_percentage = x_0 / bins
-        # list_of_frames.append(h)
-        list_of_x.append(x_percentage)
-        list_of_y.append(y_percentage)
-        list_of_entropy.append([x_percentage, y_percentage])
-        # print(list_of_entropy)
+    y_percentage = y_0 / bins
+    # list_of_frames.append(h)
+    list_of_x.append(x_percentage)
+    list_of_y.append(y_percentage)
+    list_of_entropy.append([x_percentage, y_percentage])
+    # print(list_of_entropy)
     list_of_atan.append(at := np.arctan(y_percentage / x_percentage))
     if 44 < at*180/np.pi < 50:
         print("FALL")
+    # print(y_0, x_0)
     plt.figure(1)
-    plt.plot(list_of_atan, c='r')
-    # plt.pcolormesh(q_x, q_y, h.T)
+    # plt.plot(list_of_atan, c='r')
+    plt.pcolormesh(q_x, q_y, h.T)
     # plt.figure(2)
     # plt.scatter(list_of_x, list_of_y, c="r")
     # plt.xlim((0, 1))
     # plt.ylim((0, 1))
-    plt.pause(1 / 10)
+    plt.pause(0)
     # stacked_frames = stack_frames(list_of_frames)
     # entropy_map = calculate_entropy(stacked_frames)
     # plot_heatmap(entropy_map)
-plt.pause(0)
+# plt.pause(0)
