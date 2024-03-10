93d61a0eae1c3027f11d895225a8072aac71d2bd
EXCurryBar <abo880805@gmail.com>
2024-03-10 12:38:17 +0800

    update collect_data.py automate collection process

--- collect_data.py
+++ collect_data.py
@@ -1,6 +1,8 @@
 import multiprocessing
 import time
 import os
+import numpy as np
+import pyaudio
 import traceback
 from lib.radar import Radar
 
@@ -8,6 +10,13 @@ ev = multiprocessing.Event()
 
 CLI_BAUD = 115200
 DATA_BAUD = 921600
+p = pyaudio.PyAudio()
+volume = 0.5
+fs = 44100
+duration = 1.0
+f = 440.0
+samples = (np.sin(2 * np.pi * np.arange(fs * duration) * f / fs)).astype(np.float32)
+output_bytes = (volume * samples).tobytes()
 
 
 def initialize_radar(name=None):
@@ -17,9 +26,28 @@ def initialize_radar(name=None):
                  file_name=name)
 
 
+def beep():
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=fs,
+                    output=True)
+    time.sleep(3)
+    stream.write(output_bytes)
+    # stream.stop_stream()
+    # stream.close()
+    time.sleep(5)
+    stream.write(output_bytes)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    return
+
+
 def collect_data(se, name):
     radar = initialize_radar(name)
-    while not se.is_set():
+    th = multiprocessing.Process(target=beep, args=())
+    th.start()
+    while th.is_alive():
         try:
             data_ok, frame_number, radar_data = radar.parse_data()
             if data_ok:
@@ -34,21 +62,24 @@ def collect_data(se, name):
             print(f"\nShit code: {e}")
             print(traceback.print_exc())
             break
+
     radar.close_connection()
+    return
 
 
-ACTION = 5
+ACTION = ["light_fall_lr", "light_fall_rl", "light_fall_fw", "light_fall_bw", "heavy_fall_lr", "heavy_fall_rl",
+          "heavy_fall_fw", "heavy_fall_bw", "walk_fall_lr", "walk_fall_rl", "walk_fall_fw", "walk_fall_bw",
+          "sit", "squat", "pick_thing", "walk_pass", "making_bed", "swing_hand"]
 SET = 3
 
 
 def main():
     subject = input("Enter subject name: ")
-    for i in range(ACTION):
-        action = input("enter to start collecting data")
+    for i, action in enumerate(ACTION):
         for j in range(SET):
-            t1 = multiprocessing.Process(target=collect_data, args=(ev, f"{subject}_{i}_{j}",))
+            input(f"enter to start collecting {subject}'s {action} no.{j}:")
+            t1 = multiprocessing.Process(target=collect_data, args=(ev, f"{subject}_{action}_{j}",))
             t1.start()
-            input("press enter to stop")
 
             ev.set()
             t1.join()
@@ -57,3 +88,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+    # beep()
